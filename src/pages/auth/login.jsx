67e219fb3a105cf24655eacf91f4d7fbeec6800c
theodevoid <tmangowal@gmail.com>
2022-03-16 10:08:27 +0700

    login

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,11 @@
+import { Box, Container } from "@chakra-ui/react";
+import jsCookie from "js-cookie";
+import api from "../../lib/api";
+
+const LoginPage = () => {
+  return (
+    <Container maxW="lg">
+      <Box></Box>
+    </Container>
+  );
+};
