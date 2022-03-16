80c3e6a656eb41c5802f272b68096d385399d720
theodevoid <tmangowal@gmail.com>
2022-03-16 11:03:33 +0700

    login

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -10,14 +10,75 @@ import {
   Text,
   Icon,
   Button,
+  FormControl,
+  FormHelperText,
+  useToast,
 } from "@chakra-ui/react";
 import jsCookie from "js-cookie";
 import api from "../../lib/api";
 import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
 
 const LoginPage = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const toast = useToast();
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().required("This field is required"),
+      password: Yup.string().required("This field is required"),
+    }),
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      setLoading(true);
+
+      try {
+        const res = await api.get("/users", {
+          params: {
+            username: values.username,
+            // password: values.password,
+          },
+        });
+
+        if (!res.data.length) {
+          throw new Error("User not found");
+        }
+
+        if (res.data[0].password !== values.password) {
+          throw new Error("Wrong password");
+        }
+
+        const userData = res.data[0];
+        const stringifiedUserData = JSON.stringify(userData);
+
+        jsCookie.set("user_data", stringifiedUserData);
+
+        router.push("/");
+      } catch (err) {
+        console.log(err);
+
+        toast({
+          status: "error",
+          title: "Login Failed",
+          description: err.message,
+          duration: 2000,
+        });
+
+        setLoading(false);
+      }
+    },
+  });
 
   return (
     <Container maxW="lg">
@@ -25,31 +86,50 @@ const LoginPage = () => {
         <Heading textAlign="center">Sign in to start shopping</Heading>
         <Box maxW="lg" backgroundColor="white" shadow="xl" p="8">
           <form>
-            <FormLabel htmlFor="inputUsername">Username</FormLabel>
-            <Input id="inputUsername" />
-
-            <FormLabel mt="4" htmlFor="inputPassword">
-              Password
-            </FormLabel>
-            <InputGroup>
+            <FormControl isInvalid={formik.errors.username}>
+              <FormLabel htmlFor="inputUsername">Username</FormLabel>
               <Input
-                type={passwordVisible ? "text" : "password"}
-                id="inputPassword"
-              />
-              <InputRightElement
-                children={
-                  <Icon
-                    fontSize="xl"
-                    onClick={() => setPasswordVisible(!passwordVisible)}
-                    as={passwordVisible ? IoMdEyeOff : IoMdEye}
-                    sx={{ _hover: { cursor: "pointer" } }}
-                  />
+                onChange={(event) =>
+                  formik.setFieldValue("username", event.target.value)
                 }
+                id="inputUsername"
               />
-            </InputGroup>
+              <FormHelperText>{formik.errors.username}</FormHelperText>
+            </FormControl>
 
-            <Stack>
-              <Button mt="4" colorScheme="blue">
+            <FormControl isInvalid={formik.errors.password}>
+              <FormLabel mt="4" htmlFor="inputPassword">
+                Password
+              </FormLabel>
+              <InputGroup>
+                <Input
+                  type={passwordVisible ? "text" : "password"}
+                  id="inputPassword"
+                  onChange={(event) =>
+                    formik.setFieldValue("password", event.target.value)
+                  }
+                />
+                <InputRightElement
+                  children={
+                    <Icon
+                      fontSize="xl"
+                      onClick={() => setPasswordVisible(!passwordVisible)}
+                      as={passwordVisible ? IoMdEyeOff : IoMdEye}
+                      sx={{ _hover: { cursor: "pointer" } }}
+                    />
+                  }
+                />
+              </InputGroup>
+              <FormHelperText>{formik.errors.password}</FormHelperText>
+            </FormControl>
+
+            <Stack mt="10">
+              <Button
+                onClick={formik.handleSubmit}
+                type="submit"
+                colorScheme="blue"
+                disabled={loading}
+              >
                 Login
               </Button>
             </Stack>
